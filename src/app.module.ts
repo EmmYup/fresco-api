d70f5fe7cc50e20566ce74e28f81c0b356d915ee
EmmanuelYupit <emmanuelyupit08@gmail.com>
2020-07-02 17:18:11 -0500

    orm fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { ProductModule } from './modules/product/product.module';
 import { PriceModule } from './modules/price/price.module';
 import { ConfigModule } from './config/config.module';
 import { AuthModule } from './modules/auth/auth.module';
-import * as ormconfig from '../ormConfig/ormconfig';
+import * as ormconfig from '../ormConfig/ormConfig';
 
 @Module({
   imports: [
